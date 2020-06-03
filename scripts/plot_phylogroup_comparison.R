fc083e709e5f4325d7e5041fac611ca408471c34
Boas Boas van der Putten <vdputten@test3.irc.sara.nl.irc.sara.nl>
2020-06-03 12:29:48 +0200

    Fixed phylogroup comparison plot

--- scripts/plot_phylogroup_comparison.R
+++ scripts/plot_phylogroup_comparison.R
@@ -11,7 +11,10 @@ library(ggthemes)
 plot <- read.delim(args[1])
 
 # Define labels
-phylolabels <- c("A", "B1", "B2", "C", "Cryptic\nclade I", "D", "E", "F", "Multiple")
+phylolabels <- c("A", "B1", "B2", "C", "Cryptic\nclade I", "D", "F", "Multiple")
+
+# Define breaks
+
 
 pdf(file = args[2], height = 4, width = 8)
 
@@ -20,8 +23,8 @@ ggplot(plot, aes(fill=type, y=count, x=phylogroup)) +
   geom_bar(position="dodge", stat="identity") +
   theme_clean() +
   scale_x_discrete(name = "Phylogroup/lineage", labels = phylolabels) +
-  scale_y_continuous(name = "Number of strains") +
-  scale_fill_discrete(name = "Carriage duration of \ntravel-acquired strains", labels = c("Long: >12 months (n = 14)", "Short: <1 month (n = 33)")) +
+  scale_y_continuous(name = "Number of strains", breaks = seq(0,10,2), labels = seq(0,10,2)) +
+  scale_fill_discrete(name = "Carriage duration of \ntravel-acquired strains", labels = c("Long: >12 months (n = 14)", "Short: <1 month (n = 27)")) +
   theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))
 
 dev.off()
